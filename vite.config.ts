c634b6d348a14b677206ecca34d2863b183b9e27
Darshil Vora <darshilv@gmail.com>
2026-04-05 08:12:45 -0700

    Add Preact tooling to Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'path';
+import preact from '@preact/preset-vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  plugins: [preact()],
   build: {
     outDir: 'dist',
     rollupOptions: {
